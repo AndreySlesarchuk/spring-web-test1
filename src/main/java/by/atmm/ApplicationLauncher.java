8933af6a7f08d3ba8a83f9c87758f5a812fb7221
Andrey <andrey.slesarchuk@gmail.com>
2018-06-27 17:57:11 +0300

    step 1 - creating a maven project, spring boot, the first successful launch

---
+++ src/main/java/by/atmm/ApplicationLauncher.java
@@ -0,0 +1,16 @@
+package by.atmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:andrey.slesarchuk@gmail.com">Andrey Slesarchuk</a> <br/>
+ *         Creation date: 6/27/18.
+ * @since 1.0
+ */
+@SpringBootApplication
+public class ApplicationLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLauncher.class, args);
+    }
+}
